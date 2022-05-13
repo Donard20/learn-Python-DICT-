5cb9755dad17a2943ad9c0e19eaa1108eec8641d
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-13 10:34:25 +0900

    module 2 challenge 4

---
+++ Introduction-to-Intermediate-Python/find_the _missing_2_12.py
@@ -0,0 +1,9 @@
+#challenge 4
+
+#variables
+toys = {"dolls": 4, "toy cars": 5, "legos": 50}
+
+print(toys)
+
+#print
+# {'dolls': 4, 'toy cars': 5, 'legos': 50}{'dolls': 4, 'toy cars': 5, 'legos': 50
