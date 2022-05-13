cc37e5d87a7f85b9f03a57f00aa98a31e65522db
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-13 10:22:50 +0900

    module 2 challenge 3

---
+++ Introduction-to-Intermediate-Python/add_data_dictionaries_exercise_2_12.py
@@ -0,0 +1,11 @@
+#challenge 3
+
+#create variable
+weather_in_asia = {"Japan": 22, "Malaysia":25, "Philippines": 48}
+
+weather_in_asia["Singapore"] = 27
+
+print(weather_in_asia)
+
+#print
+# {'Japan': 22, 'Malaysia': 25, 'Philippines': 48, 'Singapore': 27}
