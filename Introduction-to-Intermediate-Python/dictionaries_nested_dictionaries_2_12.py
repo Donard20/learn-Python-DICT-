268cf6630e14ea100593a9eee7689222d3b150d0
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-13 10:08:26 +0900

    module 2 challenge 1

---
+++ Introduction-to-Intermediate-Python/dictionaries_nested_dictionaries_2_12.py
@@ -0,0 +1,8 @@
+#Coding challenge
+
+##adding variables
+students = ["Joy", "Asha", "Oscar"]
+vocal_ranges = ["soprano", "alto", "tenor"]
+
+singers = dict(zip(students, vocal_ranges))
+print(singers)
