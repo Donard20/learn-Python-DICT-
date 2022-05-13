9c7b0ed3a6131fe11a1f91be369b620f3dfadf4a
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-13 10:16:47 +0900

    module 2 challenge 2

---
+++ Introduction-to-Intermediate-Python/using_item_method_2_12.py
@@ -0,0 +1,14 @@
+#Challenge 2
+
+#variables
+teacher_info = {"Name": "Rina", "Subject": "Math"}
+
+for key, value in teacher_info.items():
+    print(key)
+    print(value)
+
+#print
+# Name
+# Rina
+# Subject
+# Math
