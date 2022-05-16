22310edf50aac5de16c6a3159249cd95ab775cb5
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-16 11:23:51 +0900

    module 4 create module

---
+++ Introduction-to-Intermediate-Python/module_4_exercise_1/functionfile.py
@@ -0,0 +1,14 @@
+def get_sum(x,y):
+    return x + y
+
+def get_diff(x,y):
+    return x - y
+
+def get_product(x,y):
+    return x * y
+
+def get_division(x,y):
+    return x / y
+
+def get_modulo(x,y):
+    return x % y
---
+++ Introduction-to-Intermediate-Python/module_4_exercise_1/main.py
@@ -0,0 +1,19 @@
+# coding exercises
+# f = open("functionfile.py", "x")
+
+import functionfile as f1
+
+sum = f1.get_sum(5,6)
+print(sum)
+
+diff = f1.get_diff(5,6)
+print(diff)
+
+prod = f1.get_product(5,6)
+print(prod)
+
+div = f1.get_division(5,6)
+print(div)
+
+mod = f1.get_modulo(5,6)
+print(mod)
